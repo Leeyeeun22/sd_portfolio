500eaac90932d4ba169d61fbffab63b3be166e58
Leeyeeun <com0748@naver.com>
2025-05-07 21:02:30 +0900

    ba

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 const header = document.querySelector('.header');
+const menuBtn = document.getElementById('menuBtn');
+const nav = document.querySelector('.nav');
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('fa-xmark');
+  nav.classList.toggle('active');
+});
 
 window.onscroll = () =>{
     if(window.scrollY > 0){
